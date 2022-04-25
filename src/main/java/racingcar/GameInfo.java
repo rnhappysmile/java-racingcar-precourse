0463f56fdb616504a2901bc337467263ec8ace0d
rnhappysmile <rnhappysmile@gmail.com>
2022-04-25 20:33:11 +0900

    feat: 주어진 횟수 객체 생성

---
+++ src/main/java/racingcar/GameInfo.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+public class GameInfo {
+    public static final int MIN_COUNT = 1;
+    public static final int MAX_COUNT = Integer.MAX_VALUE;    // 요구사항에서 최대 게임수가 명시되지 않아 변경될 수 있음
+
+    private int rounds;
+
+    public GameInfo(String command) {
+        int tempCount = stringToInteger(command);
+
+        validateSize(tempCount);
+
+        this.rounds = rounds;
+    }
+
+    private int stringToInteger(String command) {
+        return Integer.parseInt(command);
+    }
+
+    private void validateSize(int rounds) {
+        if (MIN_COUNT > rounds || MAX_COUNT < rounds) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
+}
